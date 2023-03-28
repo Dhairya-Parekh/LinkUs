01477105005b2710208afe9c5aa8b9a2d402e38f
Dhairya Parekh <dhairyaparekh01@gmail.com>
2023-03-28 13:18:42 +0530

    Adding database to the backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,37 @@
 const express = require("express");
 require('dotenv').config({path:'./config.env'});
-      
+const { Client } = require('pg')
+
+const client = new Client({
+  host: process.env.DATABASE_HOST,
+  database: process.env.DATABASE_NAME,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  port: process.env.DATABASE_PORT,
+});
+
 const app = express();
 
 // set port, listen for requests
 const PORT = process.env.BACKEND_PORT || 8080;
+
+client.connect((err) => {
+  if (err) {
+    console.error('connection error', err.stack)
+    console.log("Host: ", process.env.DATABASE_HOST)
+    console.log("Database: ", process.env.DATABASE_NAME)
+    console.log("User: ", process.env.DATABASE_USER)
+    console.log("Password: ", process.env.DATABASE_PASSWORD)
+  } 
+  else {
+    console.log('connected')
+    console.log("Host: ", process.env.DATABASE_HOST)
+    console.log("Database: ", process.env.DATABASE_NAME)
+    console.log("User: ", process.env.DATABASE_USER)
+    console.log("Password: ", process.env.DATABASE_PASSWORD)
+  }
+})
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
