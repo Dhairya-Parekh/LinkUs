35040aa73b0c0c53dba614efc0cfaf5402291a98
Shikhariitb <shikhar108mundra@gmail.com>
2023-04-08 12:19:39 +0530

    added hashing

--- Backend/query.js
+++ Backend/query.js
@@ -1,6 +1,8 @@
 const { v4: uuidv4 } = require('uuid');
 require('dotenv').config({ path: './config.env' });
 const { Client } = require('pg');
+const bcrypt = require('bcrypt');
+const saltRounds = 10
 
 const client = new Client({
   host: process.env.DATABASE_HOST,
@@ -43,24 +45,31 @@ client.connect((err) => {
 const login = (body) => {
   return new Promise(function (resolve, reject) {
     const { user_name, password } = body;
-    client.query('select user_id FROM users where user_name = $1 and passcode = $2', [user_name, password], (error, results) => {
-      if (error) {
-        reject(error);
-      }
-      if (results.rows.length == 0) {
-        resolve({
-          success: false,
-          user_id: null,
-          message: "Invalid username or password"
-        });
-      }
-      else {
-        resolve({
-          success: true,
-          user_id: results.rows[0].user_id,
-          message: "Login successful"
-        });
-      }
+    bcrypt
+    .genSalt(saltRounds)
+    .then(salt => {
+      return bcrypt.hash(password, salt)
+    })
+    .then(hash =>{
+      client.query('select user_id FROM users where user_name = $1 and passcode = $2', [user_name, hash], (error, results) => {
+        if (error) {
+          reject(error);
+        }
+        if (results.rows.length == 0) {
+          resolve({
+            success: false,
+            user_id: null,
+            message: "Invalid username or password"
+          });
+        }
+        else {
+          resolve({
+            success: true,
+            user_id: results.rows[0].user_id,
+            message: "Login successful"
+          });
+        }
+      })
     })
   })
 }
@@ -76,15 +85,22 @@ const signup = (body) => {
       }
       if (results.rows.length == 0) {
         user_id = uuidv4();
-        client.query('insert into users (user_id, user_name, passcode, email_id) values ($1, $2, $3, $4)', [user_id, user_name, password, email], (error, results1) => {
-          if (error) {
-            reject(error);
-          }
-          resolve({
-            success: true,
-            user_id: user_id,
-            message: "Voila! You are now a member of our community."
-          });
+        bcrypt
+        .genSalt(saltRounds)
+        .then(salt => {
+          return bcrypt.hash(password, salt)
+        })
+        .then(hash => {
+          client.query('insert into users (user_id, user_name, passcode, email_id) values ($1, $2, $3, $4)', [user_id, user_name, hash, email], (error, results1) => {
+            if (error) {
+              reject(error);
+            }
+            resolve({
+              success: true,
+              user_id: user_id,
+              message: "Voila! You are now a member of our community."
+            });
+          })
         })
       }
       else {
