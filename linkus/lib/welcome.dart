22f404ea8fff2fc6d35c3349df364d49e1c6e7c9
Shikhar Mundra <shikhar108mundra@gmail.com>
2023-04-25 15:16:02 +0530

    welcome page changed

--- linkus/lib/welcome.dart
+++ linkus/lib/welcome.dart
@@ -4,44 +4,49 @@ class Welcome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(
-        child: Text('Welcome! Login or register to start using the App.'),
-      ),
-      bottomNavigationBar: Stack(
-        children: <Widget>[
-          Positioned(
-            bottom: 16.0,
-            left: 16.0,
-            right: 16.0,
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                elevation: 4.0,
-                padding: const EdgeInsets.all(16.0),
-                primary: Color.fromARGB(255, 194, 80, 65),
-              ),
-              child: const Text('Login'),
-              onPressed: () {
-                Navigator.pushReplacementNamed(context, '/login');
-              },
-            ),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/background.jpg'),
+            fit: BoxFit.cover,
           ),
-          Positioned(
-            bottom: 84.0,
-            left: 16.0,
-            right: 16.0,
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                elevation: 4.0,
-                padding: const EdgeInsets.all(16.0),
-                primary: Color.fromARGB(255, 22, 101, 167),
+        ),
+        child: Stack(
+          children: [
+            Positioned(
+              bottom: 16.0,
+              left: 16.0,
+              right: 16.0,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  elevation: 4.0,
+                  padding: const EdgeInsets.all(16.0),
+                  primary: const Color.fromARGB(255, 194, 80, 65),
+                ),
+                child: const Text('Login'),
+                onPressed: () {
+                  Navigator.pushReplacementNamed(context, '/login');
+                },
               ),
-              child: const Text('Register'),
-              onPressed: () {
-                Navigator.pushReplacementNamed(context, '/signup');
-              },
             ),
-          ),
-        ],
+            Positioned(
+              bottom: 84.0,
+              left: 16.0,
+              right: 16.0,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  elevation: 4.0,
+                  padding: const EdgeInsets.all(16.0),
+                  primary: const Color.fromARGB(255, 22, 101, 167),
+                ),
+                child: const Text('Register'),
+                onPressed: () {
+                  Navigator.pushReplacementNamed(context, '/signup');
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
